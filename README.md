c2d64cacec7d1e5d14462df4056cd6faf2e5101d
Mike Hall <mike@just3ws.com>
2025-10-13 10:26:01 -0500

    Cleanup and migration

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Vdots 
+# Vdots
 
 My Neovim dotfiles.
 
@@ -7,6 +7,7 @@ My Neovim dotfiles.
 ```shell
 cd
 mkdir -p ~/.local/share/nvim
+git clone --depth 1 https://github.com/wbthomason/packer.nvim ~/.local/share/nvim/site/pack/packer/start/packer.nvim
 git clone git@github.com:just3ws/vdots.git ~/.config/nvim
 ```
 
--- lua/options.lua
+++ lua/options.lua
@@ -1,10 +1,4 @@
-vim.opt.number = true
-vim.opt.numberwidth = 3
-vim.opt.ignorecase = true
-vim.opt.smartcase = true
-vim.opt.termguicolors = true
-vim.opt.splitright = true
-vim.opt.splitbelow = true
-vim.opt.mouse = "a"
-vim.opt.clipboard:append("unnamedplus")
-vim.opt.backspace = { "indent", "eol", "start" }
+local opt = vim.opt
+local g = vim.g
+
+opt.mouse = "a"
