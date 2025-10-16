be380b44ec34278d48c4f4fb966a226c641659b3
Mike Hall <mike@just3ws.com>
2025-10-16 18:32:35 -0500

    Converted autocmds and install jsregexp for luasnips

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -1,68 +1,75 @@
-require('packer').startup(function(use)
+require("packer").startup(function(use)
   -- Core dependency
-  use 'wbthomason/packer.nvim'
+  use("wbthomason/packer.nvim")
 
   -- UI
-  use 'arcticicestudio/nord-vim'
-  use { 'vim-airline/vim-airline' }
-  use { 'vim-airline/vim-airline-themes' }
+  use("arcticicestudio/nord-vim")
+  use({ "vim-airline/vim-airline" })
+  use({ "vim-airline/vim-airline-themes" })
 
   -- Editing, text objects, motion
-  use 'tpope/vim-surround'
-  use 'tpope/vim-repeat'
-  use 'tpope/vim-commentary'
-  use 'wellle/targets.vim'
-  use 'kana/vim-textobj-user'
-  use 'nelstrom/vim-textobj-rubyblock'
-  use 'tek/vim-textobj-ruby'
-  use 'vim-scripts/align'
+  use("tpope/vim-surround")
+  use("tpope/vim-repeat")
+  use("tpope/vim-commentary")
+  use("wellle/targets.vim")
+  use("kana/vim-textobj-user")
+  use("nelstrom/vim-textobj-rubyblock")
+  use("tek/vim-textobj-ruby")
+  use("vim-scripts/align")
 
   -- Ruby & Rails
-  use 'vim-ruby/vim-ruby'
-  use 'tpope/vim-bundler'
-  use 'tpope/vim-rails'
-  use 'tpope/vim-rake'
-  use 'tpope/vim-endwise'
+  use("vim-ruby/vim-ruby")
+  use("tpope/vim-bundler")
+  use("tpope/vim-rails")
+  use("tpope/vim-rake")
+  use("tpope/vim-endwise")
 
   -- Go support
   -- use { 'fatih/vim-go', run = ':GoInstallBinaries' }
 
   -- Git & workflow
-  use 'tpope/vim-fugitive'
-  use 'tpope/vim-rhubarb'
-  use 'tpope/vim-eunuch'
+  use("tpope/vim-fugitive")
+  use("tpope/vim-rhubarb")
+  use("tpope/vim-eunuch")
 
   -- Tools
-  use { 'junegunn/fzf.vim', opt = true, cmd = { 'FZF', 'Files', 'GFiles' } }
-  use 'editorconfig/editorconfig-vim'
-  use 'mhinz/vim-startify'
+  use({ "junegunn/fzf.vim", opt = true, cmd = { "FZF", "Files", "GFiles" } })
+  use("editorconfig/editorconfig-vim")
+  use("mhinz/vim-startify")
   -- use 'dense-analysis/ale'
-  use { "mfussenegger/nvim-lint" }
-  use { "stevearc/conform.nvim" }
+  use({ "mfussenegger/nvim-lint" })
+  use({ "stevearc/conform.nvim" })
 
   -- Misc
-  use 'tpope/vim-abolish'
-  use 'tpope/vim-projectionist'
+  use("tpope/vim-abolish")
+  use("tpope/vim-projectionist")
   -- use 'tpope/vim-sensible'
-  use 'pbrisbin/vim-mkdir'
-  use 'vitalk/vim-shebang'
+  use("pbrisbin/vim-mkdir")
+  use("vitalk/vim-shebang")
 
-  use {
-    'nvim-tree/nvim-tree.lua',
-    requires = { 'nvim-tree/nvim-web-devicons' },
-  }
+  use({
+    "nvim-tree/nvim-tree.lua",
+    requires = { "nvim-tree/nvim-web-devicons" },
+  })
 
   --------------------------------------------------------------------------
   -- Modern syntax and highlighting: Tree-sitter
   --------------------------------------------------------------------------
-  use {
-    'nvim-treesitter/nvim-treesitter',
-    run = ':TSUpdate',
+  use({
+    "nvim-treesitter/nvim-treesitter",
+    run = ":TSUpdate",
     config = function()
-      require('nvim-treesitter.configs').setup {
+      require("nvim-treesitter.configs").setup({
         ensure_installed = {
-          'ruby', 'go', 'lua', 'javascript', 'json',
-          'html', 'css', 'bash', 'python'
+          "ruby",
+          "go",
+          "lua",
+          "javascript",
+          "json",
+          "html",
+          "css",
+          "bash",
+          "python",
         },
         highlight = { enable = true },
         indent = { enable = true },
@@ -75,41 +82,53 @@ require('packer').startup(function(use)
             node_decremental = "grm",
           },
         },
-      }
-    end
-  }
+      })
+    end,
+  })
 
-  use {
-    'nvim-treesitter/nvim-treesitter-textobjects',
-    after = 'nvim-treesitter'
-  }
+  use({
+    "nvim-treesitter/nvim-treesitter-textobjects",
+    after = "nvim-treesitter",
+  })
 
-  use {
-    'windwp/nvim-ts-autotag',
-    after = 'nvim-treesitter',
+  use({
+    "windwp/nvim-ts-autotag",
+    after = "nvim-treesitter",
     config = function()
-      require('nvim-ts-autotag').setup()
-    end
-  }
+      require("nvim-ts-autotag").setup()
+    end,
+  })
 
-  use {
-    'JoosepAlviste/nvim-ts-context-commentstring',
-    after = 'nvim-treesitter'
-  }
+  use({
+    "JoosepAlviste/nvim-ts-context-commentstring",
+    after = "nvim-treesitter",
+  })
 
-  use 'neovim/nvim-lspconfig' -- Base LSP configuration (already used implicitly)
-  use 'williamboman/mason.nvim'
-  use 'williamboman/mason-lspconfig.nvim'
+  use("neovim/nvim-lspconfig") -- Base LSP configuration (already used implicitly)
+  use("williamboman/mason.nvim")
+  use("williamboman/mason-lspconfig.nvim")
 
   -- LSP + Completion
-  use 'hrsh7th/nvim-cmp'         -- Completion framework
-  use 'hrsh7th/cmp-nvim-lsp'     -- LSP source for nvim-cmp
-  use 'hrsh7th/cmp-buffer'       -- Buffer completions
-  use 'hrsh7th/cmp-path'         -- Filesystem paths
-  use 'hrsh7th/cmp-cmdline'      -- Command-line completions
-  use 'L3MON4D3/LuaSnip'         -- Snippet engine (required)
-  use 'saadparwaiz1/cmp_luasnip' -- Snippet completions
+  use("hrsh7th/nvim-cmp") -- Completion framework
+  use("hrsh7th/cmp-nvim-lsp") -- LSP source for nvim-cmp
+  use("hrsh7th/cmp-buffer") -- Buffer completions
+  use("hrsh7th/cmp-path") -- Filesystem paths
+  use("hrsh7th/cmp-cmdline") -- Command-line completions
 
-  use 'rafamadriz/friendly-snippets'
-  require('luasnip.loaders.from_vscode').lazy_load()
+  use({
+    "L3MON4D3/LuaSnip", -- Snippet engine (required)
+    -- follow latest release.
+    tag = "v2.*", -- Replace <CurrentMajor> by the latest released major (first number of latest release)
+    -- install jsregexp (optional!:).
+    run = "make install_jsregexp",
+    config = function()
+      -- pcall(require, "jsregexp")
+      require("luasnip.loaders.from_vscode").lazy_load()
+    end,
+  })
+
+  use("saadparwaiz1/cmp_luasnip") -- Snippet completions
+
+  use("rafamadriz/friendly-snippets")
+  require("luasnip.loaders.from_vscode").lazy_load()
 end)
