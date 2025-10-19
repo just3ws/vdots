19b1801fe56ea7f02a1edb6338ae92817eb89ce3
Mike Hall <mike@just3ws.com>
2025-10-19 15:18:09 -0500

    Cleaning up navigation

--- init.lua
+++ init.lua
@@ -1,6 +1,5 @@
 -- Detect Homebrew prefix dynamically
 local brew_prefix = vim.fn.isdirectory "/opt/homebrew" == 1 and "/opt/homebrew" or "/usr/local"
-vim.opt.runtimepath:append(brew_prefix .. "/opt/fzf")
 
 -- Disable netrw at the very start if using nvim-tree
 vim.g.loaded_netrw = 1
@@ -11,14 +10,14 @@ require "plugins"
 require "options"
 require "keymaps"
 require "autocmds"
-require "lsp"
-require "treesitter"
-require "linting"
-require "formatting"
 require "diagnostics"
-require "nvimtree"
 require "settings"
 require "filetypes"
+require "lsp"
+require "linting"
+require "formatting"
+require "treesitter"
+require "nvimtree"
 
 -- UI and appearance
 vim.opt.tags:append ".git/tags"
@@ -26,19 +25,3 @@ vim.opt.background = "dark"
 vim.cmd.colorscheme "nord"
 vim.g.airline_theme = "nord"
 vim.api.nvim_set_hl(0, "BadWhitespace", { ctermbg = "red", bg = "darkred" })
-
--- FZF integration
-vim.g.fzf_action = {
-  ["ctrl-t"] = "tab split",
-  ["ctrl-x"] = "split",
-  ["ctrl-v"] = "vsplit",
-}
-
--- Grep integration (ag → rg fallback)
-if vim.fn.executable "ag" == 1 then
-  vim.opt.grepprg = "ag --nogroup --nocolor"
-  vim.env.FZF_DEFAULT_COMMAND = "ag --literal --files-with-matches --nocolor --hidden -g ''"
-elseif vim.fn.executable "rg" == 1 then
-  vim.opt.grepprg = "rg --vimgrep"
-  vim.env.FZF_DEFAULT_COMMAND = "rg --files --hidden --glob '!.git/*'"
-end
--- lua/plugins.lua
+++ lua/plugins.lua
@@ -1,105 +1,92 @@
 -- ~/.config/nvim/lua/plugins.lua
 return require("packer").startup(function(use)
-  use("wbthomason/packer.nvim")
+  use "wbthomason/packer.nvim"
 
   -- UI & statusline
-  use({ "shaunsingh/nord.nvim" })
-  use({
+  use { "shaunsingh/nord.nvim" }
+  use {
     "nvim-lualine/lualine.nvim",
     requires = { "nvim-tree/nvim-web-devicons" },
     config = function()
-      require("lualine").setup({ options = { theme = "nord" } })
+      require("lualine").setup { options = { theme = "nord" } }
     end,
-  })
+  }
 
   -- Core editing
-  use("tpope/vim-surround")
-  use("tpope/vim-repeat")
-  use("tpope/vim-commentary")
-  use("wellle/targets.vim")
-  use("kana/vim-textobj-user")
-  use("nelstrom/vim-textobj-rubyblock")
-  use("tek/vim-textobj-ruby")
-  use("tpope/vim-eunuch")
+  use "tpope/vim-surround"
+  use "tpope/vim-repeat"
+  use "tpope/vim-commentary"
+  use "wellle/targets.vim"
+  use "kana/vim-textobj-user"
+  use "nelstrom/vim-textobj-rubyblock"
+  use "tek/vim-textobj-ruby"
+  use "tpope/vim-eunuch"
 
   -- Ruby ecosystem
-  use("vim-ruby/vim-ruby")
-  use("tpope/vim-endwise")
-  use("tpope/vim-bundler")
-  use("tpope/vim-rails")
+  use "vim-ruby/vim-ruby"
+  use "tpope/vim-endwise"
+  use "tpope/vim-bundler"
+  use "tpope/vim-rails"
 
   -- Git
-  use({ "tpope/vim-fugitive", cmd = { "Git", "Gdiffsplit", "Gblame" } })
-  use("tpope/vim-rhubarb")
+  use { "tpope/vim-fugitive", cmd = { "Git", "Gdiffsplit", "Gblame" } }
+  use "tpope/vim-rhubarb"
 
   -- File tree
-  use({
+  use {
     "nvim-tree/nvim-tree.lua",
     requires = { "nvim-tree/nvim-web-devicons" },
     config = function()
-      require("nvimtree")
+      require "nvimtree"
     end,
-  })
+  }
 
   -- Treesitter
-  use({
+  use {
     "nvim-treesitter/nvim-treesitter",
     run = ":TSUpdate",
     config = function()
-      require("treesitter")
+      require "treesitter"
     end,
-  })
-  use("nvim-treesitter/nvim-treesitter-textobjects")
-  use("windwp/nvim-ts-autotag")
-  use("JoosepAlviste/nvim-ts-context-commentstring")
+  }
+  use "nvim-treesitter/nvim-treesitter-textobjects"
+  use "windwp/nvim-ts-autotag"
+  use "JoosepAlviste/nvim-ts-context-commentstring"
 
   -- LSP / Completion
-  use({
+  use {
     "neovim/nvim-lspconfig",
     requires = {
       "williamboman/mason.nvim",
       "williamboman/mason-lspconfig.nvim",
     },
     config = function()
-      require("lsp")
+      require "lsp"
     end,
-  })
-  use("hrsh7th/nvim-cmp")
-  use("hrsh7th/cmp-nvim-lsp")
-  use("hrsh7th/cmp-buffer")
-  use("hrsh7th/cmp-path")
-  use("hrsh7th/cmp-cmdline")
+  }
+  use "hrsh7th/nvim-cmp"
+  use "hrsh7th/cmp-nvim-lsp"
+  use "hrsh7th/cmp-buffer"
+  use "hrsh7th/cmp-path"
+  use "hrsh7th/cmp-cmdline"
 
-  use({
+  use {
     "L3MON4D3/LuaSnip",
     tag = "v2.*",
     run = "make install_jsregexp",
     config = function()
       require("luasnip.loaders.from_vscode").lazy_load()
     end,
-  })
-  use("saadparwaiz1/cmp_luasnip")
-  use("rafamadriz/friendly-snippets")
+  }
+  use "saadparwaiz1/cmp_luasnip"
+  use "rafamadriz/friendly-snippets"
 
   -- Linting / formatting
-  use("mfussenegger/nvim-lint")
-  use("stevearc/conform.nvim")
+  use "mfussenegger/nvim-lint"
+  use "stevearc/conform.nvim"
 
   -- Misc
-  use("tpope/vim-abolish")
-  use("pbrisbin/vim-mkdir")
-  use("vitalk/vim-shebang")
-
-  use({
-    "goolord/alpha-nvim",
-    -- dependencies = { 'echasnovski/mini.icons' },
-    dependencies = { "nvim-tree/nvim-web-devicons" },
-    config = function()
-      local startify = require("alpha.themes.startify")
-      -- available: devicons, mini, default is mini
-      -- if provider not loaded and enabled is true, it will try to use another provider
-      startify.file_icons.provider = "devicons"
-      require("alpha").setup(startify.config)
-    end,
-  })
+  use "tpope/vim-abolish"
+  use "pbrisbin/vim-mkdir"
+  use "vitalk/vim-shebang"
 end)
