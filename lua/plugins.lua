97aba7620c149492ff76237eb141700d1e401591
Mike Hall <mike@just3ws.com>
2025-10-19 12:28:23 -0500

    Replaced startify with alpha-nvim

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -89,4 +89,17 @@ return require("packer").startup(function(use)
   use("tpope/vim-abolish")
   use("pbrisbin/vim-mkdir")
   use("vitalk/vim-shebang")
+
+  use({
+    "goolord/alpha-nvim",
+    -- dependencies = { 'echasnovski/mini.icons' },
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+    config = function()
+      local startify = require("alpha.themes.startify")
+      -- available: devicons, mini, default is mini
+      -- if provider not loaded and enabled is true, it will try to use another provider
+      startify.file_icons.provider = "devicons"
+      require("alpha").setup(startify.config)
+    end,
+  })
 end)
