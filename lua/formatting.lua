2077a94514c5bf9cc74c50af875b138a194b785c
Mike Hall <mike@just3ws.com>
2025-10-19 14:28:36 -0500

    EditorConfig

--- lua/formatting.lua
+++ lua/formatting.lua
@@ -5,16 +5,26 @@ require("conform").setup({
     json = { "prettier" },
     yaml = { "prettier" },
     css = { "prettier" },
+    lua = { "stylua" },
   },
 
-  format_on_save = {
-    timeout_ms = 2000,
-    lsp_fallback = true,
-  },
+  -- 🔧 Dynamic format_on_save: skip special files
+  format_on_save = function(bufnr)
+    local filename = vim.api.nvim_buf_get_name(bufnr)
+
+    -- skip special files
+    if filename:match("%.luacheckrc$") or filename:match("%.stylua%.toml$") then
+      return false
+    end
+
+    return {
+      timeout_ms = 2000,
+      lsp_fallback = true,
+    }
+  end,
 })
 
 -- Manual command (like :ALEFix)
 vim.api.nvim_create_user_command("Format", function()
   require("conform").format({ async = true, lsp_fallback = true })
 end, {})
-
