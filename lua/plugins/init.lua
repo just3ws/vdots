22224be3b343581134564a2cc542cfb4d9369896
Mike Hall <mike.hall@omf.com>
2025-10-22 13:20:35 -0500

    Added which-key

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -41,6 +41,24 @@ return {
     dependencies = { "kana/vim-textobj-user" },
     ft = "ruby",
   },
+  {
+    "folke/which-key.nvim",
+    event = "VeryLazy",
+    opts = {
+      -- your configuration comes here
+      -- or leave it empty to use the default settings
+      -- refer to the configuration section below
+    },
+    keys = {
+      {
+        "<leader>?",
+        function()
+          require("which-key").show { global = false }
+        end,
+        desc = "Buffer Local Keymaps (which-key)",
+      },
+    },
+  },
 
   -----------------------------------------------------
   -- Ruby ecosystem
